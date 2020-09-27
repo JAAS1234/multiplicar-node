ab9f3ef6ce9b3c0391fe9293e8162bfb29c10c9e
Dangely <jhoandange@gmail.com>
2020-09-27 16:45:00 -0500

    commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,41 @@
+//requireds
+const fs = require('fs');
+const colors = require('colors');
+
+//const fs=require('express');
+//const fs=require('./fs');
+
+let crearArchivo = (base, limite) => {
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido ${base} no es un número`);
+            return;
+        }
+        let data = '';
+        for (let i = 1; i <= limite; i++) {
+            data += `${base}*${i}=${base * i}\n`;
+        }
+
+
+        fs.writeFile(`tablas/tabla-${base}-limite-${limite}.txt`, data, (err) => {
+            if (err) reject(err)
+            else
+                resolve(`tabla-${base}-limite-${limite}.txt`);
+        });
+    });
+}
+
+let listarTabla = (base, limite) => {
+    console.log('=========================='.green);
+    console.log(`=====TABLA DE ${base}=====`.red);
+    console.log('=========================='.green);
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${base}*${i}=${base * i}`);
+    }
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
